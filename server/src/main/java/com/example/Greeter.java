fc9ee5fb6befb01fe84065b15051c85c39326dde
benBLH <82593556+benBLH@users.noreply.github.com>
2021-04-16 15:01:19 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -8,7 +8,10 @@ public class Greeter {
 
   }
 
- 
+  /**
+  *@param   brzgrzgrsrzh
+  *@return   dbsbsbsb
+  */
   
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
